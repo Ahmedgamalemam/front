02587db436653b87a7354aa2c31092e4f8d439b7
Hebaa252022 <hebaali252022@gmail.com>
2023-02-25 20:32:36 +0200

    EditBrofile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditProfileComponent } from './component/edit-profile/edit-profile.component';
 import { HomeComponent } from './component/home/home.component';
 import { ProfileComponent } from './component/profile/profile.component';
 
 const route: Routes = [
 {path:'',component:HomeComponent},
 {path:'home',component:HomeComponent},
+{path:'EditProfile',component:EditProfileComponent},
+{ path: "profile", component: ProfileComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './share/navbar/navbar.component';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './component/home/home.component';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import lottie from 'lottie-web';
 import { defineElement } from 'lord-icon-element';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { RouterModule,Routes } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProfileComponent } from './component/profile/profile.component';
 import { FooterComponent } from './share/footer/footer.component';
-var route:Routes = [{path:"home",component:HomeComponent},
-{path:"profile",component:ProfileComponent}
-]
+import { EditProfileComponent } from './component/edit-profile/edit-profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    ProfileComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -35,13 +35,12 @@ var route:Routes = [{path:"home",component:HomeComponent},
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatSlideToggleModule,
-    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { 
+export class AppModule {
   constructor() {
     defineElement(lottie.loadAnimation);
   }
