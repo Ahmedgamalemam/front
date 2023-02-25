79d597757f89450d339cefe71747a488f8b0a982
NadaZfarid <nadazfarid@gmail.com>
2023-02-25 19:47:01 +0200

    Login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 import { NavBarService } from 'src/app/core/Services/nav-bar.service';
 
 @Component({
@@ -7,10 +9,19 @@ import { NavBarService } from 'src/app/core/Services/nav-bar.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(public nav:NavBarService){
-
-  }
+  Emailerror=false;
+  Passworderror=false;
+  form= new FormGroup({
+    Email:new FormControl(null,[Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),Validators.required]),
+    Password:new FormControl(null,Validators.required)
+  })
+  constructor(public nav:NavBarService,public route:Router){}
   ngOnInit(){
     this.nav.hide();
+    console.log(this.form)
+  }
+
+  Login(){
+      this.route.navigate(["/home"])
   }
 }
