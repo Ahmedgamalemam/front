0fe7f3ef98db5d7e93f7feeb9088748a49c1ea17
NadaZfarid <nadazfarid@gmail.com>
2023-02-27 18:06:24 +0200

    n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { MarkerService } from './core/Services/marker.service';
 import { PopupService } from './core/Services/popup.service';
 import { MapComponent } from './component/map/map.component';
 import { PetsComponent } from './component/pets/pets.component';
-import { ProductsComponent } from './products/products.component';
+import { ProductsComponent } from './component/products/products.component';
 @NgModule({
   declarations: [
     AppComponent,
