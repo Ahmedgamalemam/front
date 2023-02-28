b56a47fe92a52b3a6d7f450c52b050d2d76dedf8
Gharam Ahmed Mohamed <romaahmed250@gmail.com>
2023-02-28 21:14:51 +0200

    ProductsBack

--- src/app/share/navbar/navbar.component.ts
+++ src/app/share/navbar/navbar.component.ts
@@ -1,22 +1,34 @@
-import { Component,HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavBarService } from 'src/app/core/Services/nav-bar.service';
 import { SearchService } from 'src/app/core/Services/search.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  textsearch: string = "";
+  constructor(
+    public nav: NavBarService,
+    public search: SearchService,
+    public searchservice: NavBarService,
 
-constructor(public nav:NavBarService,public search:SearchService) {
+  ) {}
 
-}
-
-  sidebar(){
-  document.getElementById("side")?.classList.toggle("show-side")
+  applyFilter(event: Event) {
+    let inputsearch = (event.target as HTMLInputElement).value;
+    this.textsearch = inputsearch;
   }
-  close(){
-    document.getElementById("side")?.classList.toggle("show-side")
+  categorysearch() {
+    this.searchservice.setsearch(this.textsearch);
+    //console.log(this.textsearch);
+  }
+
+  sidebar() {
+    document.getElementById('side')?.classList.toggle('show-side');
+  }
+  close() {
+    document.getElementById('side')?.classList.toggle('show-side');
   }
 }
